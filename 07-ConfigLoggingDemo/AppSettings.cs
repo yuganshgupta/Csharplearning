6021dd02cba3547f6bab6dbf03f933c895cf89c6
Yugansh Gupta <yugansh.gupta1234@gmail.com>
2026-09-11 02:39:39 +0530

    Add configuration and appsettings demo

---
+++ 07-ConfigLoggingDemo/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_ConfigLoggingDemo;
+
+class AppSettings
+{
+    public string AppName { get; set; } = "";
+    public int MaxRetries { get; set; }
+}
---
+++ 07-ConfigLoggingDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace _07_ConfigLoggingDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("appsettings.Local.json")
+            .Build();
+
+        //string? appname = configuration["AppName"]; // here configuration represented the whole root section 
+        //int maxRetries = configuration.GetValue<int>("MaxRetries");
+
+
+        // var appSettings = configuration.GetSection("AppSettings");
+
+        // string? appName = appSettings["AppName"]; // now this is inside appsettings and its a nested configuration 
+        // int maxRetries = appSettings.GetValue<int>("MaxRetries");
+
+        AppSettings? settings = configuration // take only this part of config
+        .GetSection("AppSettings") // create an AppSettings object
+        .Get<AppSettings>(); // matches AppName -> AppName and MaxRetries -> MaxRetries
+
+        Console.WriteLine(settings?.AppName);
+        Console.WriteLine(settings?.MaxRetries);
+
+    }
+}
