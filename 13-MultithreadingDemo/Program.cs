53129e703ab439c2b6c412594e52d316bd018a3e
Yugansh Gupta <yugansh.gupta1234@gmail.com>
2026-09-19 02:39:28 +0530

    Add multithreading fundamentals and synchronization demo

---
+++ 13-MultithreadingDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _13_MultithreadingDemo;
+using System.Threading;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Thread worker = new Thread(Work);
+
+
+        //worker.Start();
+        //worker.Join();
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.WriteLine($"Main {i} running");
+        //    Thread.Sleep(10);
+        //}
+
+       
+        
+         RaceConditionDemo.Run();
+        
+
+
+
+    }
+
+    static void Work()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Worker {i} running");
+            Thread.Sleep(10);
+        }
+    }
+}
---
+++ 13-MultithreadingDemo/RaceConditionDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+
+namespace _13_MultithreadingDemo;
+
+internal static class RaceConditionDemo
+{
+    private static int counter = 0;
+    //private static readonly object counterLock = new object();
+
+    internal static void Run()
+    {
+        Thread t1 = new Thread(IncreaseCounter);
+        Thread t2 = new Thread(IncreaseCounter);
+        t1.Start();
+        t2.Start();
+        t1.Join();
+        t2.Join();
+        Console.WriteLine(counter);
+        
+    }
+
+    private static void IncreaseCounter()
+    {   
+        for (int i = 0; i < 1000000; i++)
+        {
+            //lock (counterLock) lock method of preventing race condition
+            //{                  useful when we have multiple conditions that need to be executed
+            //    counter++;    when those steps depend on each other so you usually protect the whole sequence
+            //}
+            Interlocked.Increment(ref counter); //It is ideal for things
+                                                //like:Increment, decrement, exchange a value
+                                                //, and perform compare-and-swap style updates.
+
+        }
+
+        //Console.WriteLine(counter);
+    }
+}
